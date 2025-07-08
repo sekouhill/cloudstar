355773779a5e443dfc26d37437a6b46043086642
sekouhill <sekouh@gmail.com>
2025-07-07 21:32:57 -0400

    first commit

--- 7a-compute.tf
+++ 7a-compute.tf
@@ -1,27 +1,21 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance 
 resource "google_compute_instance" "iowahq-vm" {
   name         = "iowahq-vm"
   machine_type = "e2-medium"
-
-# Zone argument required if default provider zone not set 
   zone         = "us-central1-a"
 
-# Create persistent disk set as boot disk from the following image
-  boot_disk {
+boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
     }
   }
 
-# Network configurations  
-  network_interface {
-    subnetwork = google_compute_subnetwork.hqinternal.name
+network_interface {
+    subnetwork = google_compute_subnetwork.hqinternal.name 
     access_config {
       // Ephemeral public IP 
     }
   }
 
-# Use file() to get shell script for startup script argument
   metadata_startup_script = file("./startup.sh")
 
 }
