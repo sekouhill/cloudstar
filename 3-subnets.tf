f8d36575cdd664d3ea4abe3f7c912fec4a1eda72
sekouhill <sekouh@gmail.com>
2025-07-30 18:48:56 -0400

    first commit

--- 3-subnets.tf
+++ 3-subnets.tf
@@ -8,8 +8,8 @@ resource "google_compute_subnetwork" "hqinternal" {
 
 resource "google_compute_subnetwork" "app01" {
   name                     = "app01"
-  ip_cidr_range            = "10.100.10.0/24"
-  region                   = "us-central1"
+  ip_cidr_range            = "10.100.9.0/24"
+  region                   = "southamerica-east1"
   network                  = google_compute_network.prod1.id
   private_ip_google_access = true
 }
---
+++ 7b-template.tf
@@ -0,0 +1,25 @@
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_region_instance_template
+# # https://developer.hashicorp.com/terraform/language/functions/file
+# # Google Compute Engine: Regional Instance Template
+# resource "google_compute_region_instance_template" "app01" {
+#   name         = "app01-template-terraform"
+#   description  = "This template is used to clone our vm"
+#   machine_type = "e2-medium"
+
+#   # Create a new disk from an image and set as default boot disk
+#   disk {
+#     source_image = "debian-cloud/debian-12"
+#     boot         = true
+#   }
+
+#   # Network configurations  
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.hqinternal.id
+#     /*access_config {
+#       # Include this section to give the VM an external IP address
+#     } */
+#   }
+
+# # Use file() to get shell script for startup script argument
+#   metadata_startup_script = file("./startup.sh")
+# }
